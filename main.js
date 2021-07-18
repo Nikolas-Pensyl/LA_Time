9e40aa56357ad4681ba983e17309f5cfe9013eaa
Nikolas-Pensyl <45053327+Nikolas-Pensyl@users.noreply.github.com>
2021-07-17 21:54:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function activateCountdown(element) {
      element.querySelector(".until__numeric--seconds").textContent = duration.seconds().toString().padStart(2, "0");
      element.querySelector(".until__numeric--minutes").textContent = duration.minutes().toString().padStart(2, "0");
      element.querySelector(".until__numeric--hours").textContent = duration.hours().toString().padStart(2, "0"); 
-     element.querySelector(".until__numeric--days").textContent = duration.asDays().toString().padStart(2, "0");
+     element.querySelector(".until__numeric--days").textContent = duration.days().toString().padStart(2, "0");
   }, 250);
 }
 
